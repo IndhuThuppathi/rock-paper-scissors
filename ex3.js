e86aa4fc642d08e37b384ad55a8c505d7294db37
IndhuThuppathi <thuppathiindhu8@gmail.com>
2025-05-31 20:46:10 +0530

    Initial commit with all project files

---
+++ ex3.js
@@ -0,0 +1,27 @@
+'My name is:'
+'My name is:'
+'Indhu'
+'Indhu'
+'My name is:'+'Indhu'
+'My name is:Indhu'
+'Total cost :$'+ (5+3) 
+'Total cost :$8'
+`Total cost : $${(5+3)}`
+'Total cost : $8'
+alert(`Total cost : $${(5+3)}`);
+undefined
+'Total cost: $'+(599+295)/100
+'Total cost: $8.94'
+`Total cost: $${((599+295)/100)}`
+'Total cost: $8.94'
+alert(`Total cost: $${((599+295)/100)}`);
+undefined
+alert(`Total cost: $${((599+295)/100)}\nThank you`);
+undefined
+`Items (${(2+2)}):$${(2*2095+2*799)/100}`
+'Items (4):$57.88'
+`Shipping & handling:$${(998)/100}`
+'Shipping & handling:$9.98'
+`Total before tax:$${(6786)/100}`
+
+'Total before tax:$67.86'
